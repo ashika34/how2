cfb8cdeec5df08a9f35d90fa27942ad810e076d8
ashika34 <ashikaspraveen@gmail.com>
2024-01-08 15:56:10 +0530

    4th commit

---
+++ String.dart
@@ -0,0 +1,13 @@
+void main(){
+  String str="Ashika";
+  print(str);
+  String str1="Mariya";
+  print(str);
+  print(str.length);
+  print(str1.toLowerCase());
+  print(str.toUpperCase());
+  print(str.isEmpty);
+  print(str1.isNotEmpty);
+  print(str.indexOf("A"));
+  print(str1.substring(2));
+}
