288fa8113c0b7fb248fe4ad90e83be3f0ad330ca
ashika34 <ashikaspraveen@gmail.com>
2024-01-08 16:19:50 +0530

    5th commit

---
+++ number.dart
@@ -0,0 +1,66 @@
+// void main(){
+//   int a=10;
+//   int b=20;
+//   int sum =a+b;
+//   print(sum);
+// }
+
+// void main(){
+//   int studentCount=20;
+//   print(studentCount);
+//   print(studentCount.isEven);
+//   print(studentCount.isOdd);
+
+// }
+ 
+//  void main(){
+//   double avgMark=10.88;
+//   print(avgMark);
+//   print(avgMark.round());
+//  }
+
+// void main(){
+//   dynamic a=100;
+//   dynamic b=100;
+//   print(a+b);
+// }
+// void main(){
+//   num a=10.54;
+//   num b=44;
+//   print(a);
+//   print(b);
+// }
+
+// void main(){
+//   String str="hai";
+//   String str1="how r u";
+//   bool val= (str!=str1);
+//   print(val);
+// }
+
+// void main(){
+//   Map<String,int>varbl=
+//   {
+//     "name":100,
+//     "Age":29,
+//   };
+//   print(varbl);
+// }
+
+// void main(){
+//   Map<String,dynamic> str1={
+//   "name":"Ashika",
+//   "age":27
+//   };
+//   print(str1);
+//   str1.addAll({"email":"ashika@gmail.com"});
+//   str1.remove("age");
+//   print(str1);
+// }
+// void main()
+// {
+// List<String>studentList=['ashika','sanu','ram'];
+// print(studentList);
+// print(studentList[2]);
+
+// }
